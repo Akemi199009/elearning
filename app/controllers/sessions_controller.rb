a8bbb51af02a10d942a8487bb0f395df404a259e
古田島明美 <kotajimaakemi@Akm.local>
2019-09-05 13:05:06 +0800

    Create Signup Login Function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,19 @@ class SessionsController < ApplicationController
 
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+
+    if user && user.authenticate(params[:session][:password])
+      login(user)
+      flash[:info] = "Success fully login!"
+      redirect_to root_path
+    else
+      flash.now[:danger] = "Invalid email or password combination!"
+      render "new"
+    end
+  end
+
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,37 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Success registered!"
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params) #カラムを全部更新するためにには　attributes
+      flash[:success] = "Editing was successful."
+      redirect_to root_path
+    else
+      render "edit"
+    end
+  end
+
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
