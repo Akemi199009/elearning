0794cc7d24c64a239084afdfe9ad5efc44ca707a
Akemi199009 <54784690+Akemi199009@users.noreply.github.com>
2019-09-25 10:12:25 +0800

    Create words (#9)
    * Create Activities Table and function
    
    * add order desc
    
    * words created
    
    * Create Activities Table and function
    
    * add order desc
    
    * words created
    
    * create learned words

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,16 +1,16 @@
 class CategoriesController < ApplicationController
 
   def index
-    
-    if params[:status] == "learned"
-      @categories = Lesson.joins(:user).find_by(user_id: current_user.id)
-      @categories.paginate(page: params[:page], per_page: 6)
-    elsif params[:status] == "anlearned"
-      @categories = Category.find_by(id: 2)
-    else
-      @categories = Category.paginate(page: params[:page], per_page: 6)
+    @categories = Category.paginate(page: params[:page], per_page: 6)
 
-    end
+    # if params[:status] == "learned"
+    #   @categories = Lesson.joins(:user).find_by(user_id: current_user.id)
+    #   @categories.paginate(page: params[:page], per_page: 6)
+    # elsif params[:status] == "anlearned"
+    #   @categories = Category.find_by(id: 2)
+    # else
+
+    # end
 end
 
 
