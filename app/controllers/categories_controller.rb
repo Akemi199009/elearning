7dfefa9b5c3f5a723b40d5469b4642d29b8c5d24
古田島明美 <kotajimaakemi@Akm.local>
2019-09-25 09:29:43 +0800

    create learned words

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,16 +1,14 @@
 class CategoriesController < ApplicationController
 
   def index
-    @categories = Category.paginate(page: params[:page], per_page: 6)
 
-    # if params[:status] == "learned"
-    #   @categories = Lesson.joins(:user).find_by(user_id: current_user.id)
-    #   @categories.paginate(page: params[:page], per_page: 6)
+    if params[:status] == "learned"
+      @categories = Category.joins(:lessons).where(lessons: {user_id: current_user.id}).paginate(page: params[:page], per_page: 6)
     # elsif params[:status] == "anlearned"
-    #   @categories = Category.find_by(id: 2)
-    # else
-
-    # end
+    #   @categories = Category.joins(WHERE NOT 'lessons'.'user_id' = 'current_user.id';).where(lessons: {result: nil}).paginate(page: params[:page], per_page: 6)
+    else
+      @categories = Category.paginate(page: params[:page], per_page: 6)
+    end
 end
 
 
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,19 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
+      if params[:status] == "words"
+        @answers = Answer.joins(:lesson).where(lessons: {user_id: current_user}).paginate(page: params[:page], per_page: 8)
+        @categories = Category.joins(:lessons).where(lessons: {user_id: current_user})
+        if params[:status] == "words" && params[:category_id]
+        @answers = Answer.joins(:lesson).where(lessons: {category_id: params[:category_id]})
+        end
+      else
       @activities = Activity.order(created_at: :desc).paginate(page: params[:page], per_page: 8)
-
+      @answers = Answer.joins(:lesson).where(lessons: {user_id: current_user})
+      end
       render 'users/home_feed'
     end
   end
+
+
 end
